cbbca0b25fadb959cfc4c66551e341eaae59c314
mTvare6 <austin76912@gmail.com>
2021-05-09 12:46:16 +0530

    Setup sharding and shard ping command

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,21 @@ use std::{
 use serenity::{
     async_trait,
     model::{channel::
-            {Message,
+        {
+            Message,
             MessageType,
             ReactionType},
-        gateway::Ready
-    },
-    gateway::Shard,
+            gateway::Ready
+        },
     client::bridge::gateway::{ShardManager, ShardId, GatewayIntents},
     prelude::*,
 };
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
 
 struct Handler;
 
@@ -32,17 +38,44 @@ impl EventHandler for Handler {
         else if msg.content == PREFIX.to_owned()+"ping" {
             let old = Instant::now();
             let mut msg:Message = msg.channel_id.say(&ctx.http, "Ping!").await.unwrap();
-            let new = Instant::now();
+            let data = ctx.data.read().await;
 
-            if let Err(why) = msg.edit(&ctx, |m| m.content(format!("Https Ping {}ms\nGateway Ping {}ms", (new - old).as_millis(), "{}"))).await{
+            let shard_manager = match data.get::<ShardManagerContainer>() {
+                Some(v) => v,
+                None => {
+                    msg.reply(&ctx, "There was a problem getting the shard manager").await.unwrap();
+                    return;
+                },
+            };
+
+            let manager = shard_manager.lock().await;
+            let runners = manager.runners.lock().await;
+
+            let runner = match runners.get(&ShardId(ctx.shard_id)) {
+                Some(runner) => runner,
+                None => {
+                    msg.reply(&ctx,  "No shard found").await.unwrap();
+                    return;
+                },
+            };
+            if let Err(why) = msg.edit(&ctx, |m| m.content(format!("Https Ping: {:?}\nShard Ping: {:?}", old.elapsed(), runner.latency.unwrap()))).await{
                 println!("Error editting message: {:?}", why);
             };
 
         }
         else if msg.content == PREFIX.to_owned()+"invite"{
-             if let Err(why) = msg.channel_id.say(&ctx.http, "https://discord.gg/ypuZfadw8H").await{
-                 println!("Error responding to message: {:?}", why)
-             }
+            if let Err(why) = msg.channel_id.say(&ctx.http, "https://discord.gg/ypuZfadw8H").await{
+                println!("Error responding to message: {:?}", why)
+            }
+        }
+        else if msg.content == PREFIX.to_owned()+"quit"{
+            let data = ctx.data.read().await;
+            if let Some(manager) = data.get::<ShardManagerContainer>() {
+                msg.reply(&ctx, "Shutting down!").await.unwrap();
+                manager.lock().await.shutdown_all().await;
+            } else {
+                msg.reply(&ctx, "There was a problem getting the shard manager").await.unwrap();
+            }
         }
     }
 
@@ -62,6 +95,12 @@ async fn main() {
         .intents(GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES)
         .await
         .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
